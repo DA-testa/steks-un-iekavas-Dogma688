61ccfe984ca7bbd77a6679cabdd741b0fc5f261e
Maksims Makarskis 221RDB380 <77271405+Dogma688@users.noreply.github.com>
2023-02-19 14:06:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,18 +13,21 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
-            opening_brackets_stack.append(Bracket(next,i+1))
-          
+            # Process opening bracket, write your code here
+            opening_brackets_stack.append(Bracket(next, i+1))
+
 
         if next in ")]}":
-           if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
-                return i+1
-            
-           opening_brackets_stac.pop()
-            
-    if opening_brackets_stac:
-        return opening_brackets_stac[0].position
-    return "success"
+            # Process closing bracket, write your code here
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+                return i + 1
+            opening_brackets_stack.pop()
+
+    if opening_brackets_stack:
+        return opening_brackets_stack[0].position
+    return "Success"
+
+
 
 
 def main():
@@ -34,5 +37,5 @@ def main():
     print(mismatch)
 
 
-if __name__ == "__main__":
+if name == "main":
     main()
